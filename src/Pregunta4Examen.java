bab982e9184598cc6ad8394c7e121c65de1ae12f
kshmr <kshmr.music@gmail.com>
2020-12-11 14:17:17 +0100

    asdf

--- src/Pregunta4Examen.java
+++ src/Pregunta4Examen.java
@@ -2,7 +2,7 @@
 public class Pregunta4Examen {
 
 	public static void main(String[] args) {
-		System.out.println("Quiero que lleven las vacaciones");
+		System.out.println("lleven las vacaciones");
 	}
 
 }
